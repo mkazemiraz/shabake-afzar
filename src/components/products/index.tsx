fb2b76146306fe3782498c22e8864355000dd1f8
mohamad <kazemiraz72@gmail.com>
2023-06-27 16:09:12 +0330

    ag-grid with simple filter

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -1,66 +1,58 @@
-import IPaginate from "@cmp/common/pagination";
-import ITable from "@cmp/common/table";
-import { Stack } from "@mui/material";
-import {
-  useGetAllProductQuery,
-  useGetPagingProductMutation,
-} from "@redux/api/product";
-import { useState, useEffect } from "react";
+import { useGetAllProductQuery } from "@redux/api/product";
+import { useCallback, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { Box } from "@mui/material";
+
 const Prodcuts = () => {
+  const gridRef = useRef<AgGridReact>(null);
   const { t } = useTranslation();
-  //   const { data, isLoading, isError } = useGetAllProductQuery({
-  //     options: {
-  //       Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //     },
-  //   });
+  const { data } = useGetAllProductQuery({
+    options: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
 
-  const [updateProducts, { data, isLoading, isError }] =
-    useGetPagingProductMutation();
-
-  const [page, setPage] = useState<number>(1);
-  useEffect(() => {
-    updateProducts({
-      options: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-      queryParams: {
-        limit: 10,
-        skip: (page - 1) * 10,
-      },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onGridReady = useCallback((params: any) => {
+    params.api.sizeColumnsToFit();
+    window.addEventListener("resize", function () {
+      setTimeout(function () {
+        params.api.sizeColumnsToFit();
+      });
     });
-  }, [page, updateProducts]);
-  return (
-    <>
-      <ITable
-        emptyTableMessage={t("ProductNotFound")}
-        tableColumns={[
-          { field: "id", title: `${t("id")}` },
-          { field: "title", title: `${t("title")}` },
-          { field: "price", title: `${t("price")}` },
-        ]}
-        tableData={data?.products}
-        isLoading={isLoading}
-        isError={isError}
-        tableLabel="products"
-      />
+    gridRef.current?.api?.sizeColumnsToFit();
+  }, []);
 
-      {data && data.total > 0 && (
-        <Stack
-          spacing={2}
-          justifyContent={"center"}
-          flexDirection={"row"}
-          mt={3}
-        >
-          <IPaginate
-            count={Math.ceil(data?.total / 10)}
-            onChange={(_event, page: number) => setPage(page)}
-            page={page}
-          />
-        </Stack>
-      )}
-    </>
+  return (
+    <Box sx={{ height: 600 }}>
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "100%", width: "100%" }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          rowData={data?.products}
+          columnDefs={[
+            {
+              field: "id",
+              filter: "agTextColumnFilter",
+              headerName: t("id"),
+              floatingFilterComponentParams: {
+                suppressFilterButton: true,
+              },
+            },
+            { field: "title", filter: true, headerName: t("title") },
+            { field: "price", filter: true, headerName: t("price") },
+          ]}
+          onGridReady={onGridReady}
+          enableRtl={true}
+        />
+      </div>
+    </Box>
   );
 };
 
